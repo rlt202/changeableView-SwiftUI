f52aa7aa3740f2ff0a89cf5e6cd3cbc57857b56a
rlt202 <danik.nikulin@gmail.com>
2020-12-15 14:02:51 +0300

    Initial Commit

---
+++ changeableView SwiftUI/changeableView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  changeableView_SwiftUIApp.swift
+//  changeableView SwiftUI
+//
+//  Created by Даниил Никулин on 15.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct changeableView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
